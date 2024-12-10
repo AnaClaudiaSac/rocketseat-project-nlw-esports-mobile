8a0554749a1d554aba7ab22b7cec0653db2649ab
AnaclaudiaSac <anaclaudiasacute75@gmail.com>
2024-12-10 12:56:30 -0300

    feat: Includes Game Screen and navigation  installation packages

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -26,7 +26,7 @@ export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
-        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+        <LinearGradient style={styles.footer} colors={THEME.COLORS.FOOTER}>
           <Text style={styles.name}> {data.title}</Text>
           <Text style={styles.ads}> {data._count.ads} anúncios</Text>
         </LinearGradient>
