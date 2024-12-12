2e0484f69c8e33ad44dfefddaf68b8f3d4d3b695
AnaclaudiaSac <anaclaudiasacute75@gmail.com>
2024-12-12 14:39:17 -0300

    feat: include GameParams in navigation and Game

--- src/screens/Game/Game.tsx
+++ src/screens/Game/Game.tsx
@@ -1,8 +1,16 @@
-import { styles } from "./styles";
+import { useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import { styles } from "./styles";
+
 import { Background } from "../../components/Background/Background";
+import { GameParams } from "@/src/@types/navigation";
 
 export function Game() {
+  const route = useRoute();
+  const game = route.params as GameParams;
+  console.log(game);
+
   return (
     <Background>
       <SafeAreaView style={styles.container}></SafeAreaView>;
